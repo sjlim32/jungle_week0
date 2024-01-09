09174b7ca2b28385e17cc3ade5a5dfc09997e392
sjlim32 <32sjlim@gmail.com>
2024-01-09 17:35:08 +0900

    FEAT: 회원가입, 로그인 페이지

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
+@app.route("/user/login")
+def logInPage():
+  return render_template('logIn.html')
+
+@app.route("/user/signup")
+def signUpPage():
+  return render_template('signUp.html')
+
+@app.route("/main/list")
+def mainPage():
+  return render_template('mainPage')
+
 # ! Mac 환경에선 port 번호 5001, 배포 시에는 5000으로 수정
 if __name__ == "__main__":
   app.run('0.0.0.0', port=5001, debug=True)
