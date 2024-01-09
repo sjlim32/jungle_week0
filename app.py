08a6080c1d1bfab41789edc799640404082de976
sjlim32 <32sjlim@gmail.com>
2024-01-09 09:28:32 +0900

    FIX: flask 환경 수정, 커밋 규칙 설정

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Hello, Flask!"
+  return render_template('index.html')
 
 # ! Mac 환경에선 port 번호 5001, 배포 시에는 5000으로 수정
 if __name__ == "__main__":
