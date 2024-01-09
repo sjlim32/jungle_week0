b68caa3fdc5a864cf42035bb4107c80b00d2afd3
hanana21 <aaa212188@gmail.com>
2024-01-09 17:50:26 +0900

    회원가입

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
 from flask import Flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.jungle_week0
 
 app = Flask(__name__)
+
+# 회원가입
+@app.route('/user/singup', methods=['get'])
+def singup():
+  Name = request.args.get('Name')
+  Id = request.args.get('Id')
+  Pw = request.args.get('Pw')
+  Nickname = request.args.get('Nickname')
+  Myself = request.args.get('Myself')
+  Img = request.args.get('Img')
+  doc ={
+    "Name":Name,
+    "Id":Id,
+    "Pw":Pw,
+    "Nickname":Nickname,
+    "Myself":Myself,
+    "Comment":" ",
+    "Img":Img,
+    "Gkeyword": [{'키워드1':0},{'키워드2':0}],
+    "Bkeyword": [{'키워드3':0},{'키워드4':0}],
+    "Writed": " "
+  }
+  db.user.insert_one(doc)
+
+  # 키워드, 코멘트 넣기
+# @app.route('/user/comment', methods=['post']) 
+# def comment(Id):
+#   input_gkeyword = request.args.get('Gkeyword')
+#   input_bkeyword = request.args.get('Bkeyword')
+#   input_comment = request.args.get('Comment')
+#   db.users.update_one({'name':Id},{'$set':{'Comment':input_comment}})
 
 @app.route("/")
 def home():
