1e7a6f18eefa0f3cdfc28a5a0896d64da9abe6be
sjlim32 <32sjlim@gmail.com>
2024-01-11 01:08:17 +0900

    CSS: 상단 배너 및 css 수정

--- routes/main.py
+++ routes/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
@@ -14,7 +14,7 @@ def mainPage():
 
     for data in searched_data:
         gkeywords = [', '.join(list(d.keys())) for d in data['Gkeyword']]
-        new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3]})
+        new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3], "Img": data['Img']})
 
     return render_template('mainPage.html', new_data=new_data)
 
@@ -27,29 +27,29 @@ def searchingPage():
 
     for data in searched_data:
         gkeywords = [', '.join(list(d.keys())) for d in data['Gkeyword']]
-        new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3]})
+        new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3], "Img": data['Img']})
 
     return render_template('mainPage.html', new_data=new_data)      
 
 # 카테고리 정렬: 가나다순
-@main_bp.route('/user/feature/listsort1', methods=['post'])
+@main_bp.route('/namesort', methods=['post'])
 def listsort1():
   searched_data = list(db.user.find({}).sort('Name', 1))
   new_data = []
   for data in searched_data:
     gkeywords = [', '.join(list(d.keys())) for d in data['Gkeyword']]
     print(gkeywords)
-    new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3]})
+    new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3], "Img": data['Img']})
     print(new_data)
   return render_template('mainPage.html', new_data=new_data)
 
 # 한 줄평의 개수를 기준으로 정렬
-@main_bp.route('/user/feature/listsort2', methods=['post'])
+@main_bp.route('/commentsort', methods=['post'])
 def listsort2():
     searched_data = list(db.user.find({}))
     sorted_data = sorted(searched_data, key=lambda x: len(x.get('Comment', [])), reverse=True)
     new_data = []
     for data in sorted_data:
         gkeywords = [', '.join(list(d.keys())) for d in data['Gkeyword']]
-        new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3]})
+        new_data.append({"Name": data["Name"], "Gkeyword": gkeywords[:3], "Img": data['Img']})
     return render_template('mainPage.html', new_data=new_data)    
